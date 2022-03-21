0bf760f8a86daf9c89f902f513c37c269a6cf468
betelgeuse <andrii.samchuk@crunch.is>
2022-03-21 06:09:06 +0200

    Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { AccentText } from './components/ui/AccentText/AccentText';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: any = null;
+
+beforeEach(() => {
+  // Setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // Cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders with or without a name', () => {
+  act(() => {
+    render(<AccentText>Text</AccentText>, container);
+  });
+  expect(container.textContent).toBe('Text');
 });
